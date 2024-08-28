69bf2445e4918e06e2ac048ebc71b48e959ba153
Provost Aurelien <Provostaurelien@gmail.com>
2024-08-29 00:16:42 +0200

    Ajout du js login + indentation + schisme js + validateur

---
+++ assets/index.js
@@ -0,0 +1,110 @@
+async function RecuperationImages() {
+  try {
+    const response = await fetch(`http://localhost:5678/api/works`);
+    const images = await response.json();
+
+    const container = document.querySelector(".gallery");
+
+    for (let i = 0; i < images.length; i++) {
+      const fig = document.createElement("figure");
+
+      const imgelement = document.createElement("img");
+      imgelement.src = images[i].imageUrl;
+      imgelement.alt = images[i].title;
+      imgelement.className = images[i].category.name;
+
+      const figcaption = document.createElement("figcaption");
+      figcaption.innerText = images[i].title;
+
+      fig.appendChild(imgelement);
+      fig.appendChild(figcaption);
+      container.appendChild(fig);
+    }
+
+    return images; // Retourne les images
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error);
+  }
+}
+
+// Fonction générique pour filtrer et afficher les images
+function afficherImagesFiltrees(images, categorie) {
+  let imagesFiltrees;
+  //Gestion des catégories avec if pour le bouton tous
+  if (categorie) {
+    imagesFiltrees = images.filter(function (image) {
+      return image.category.name === categorie;
+    });
+  } else {
+    imagesFiltrees = images;
+  }
+
+  const container = document.querySelector(".gallery");
+  container.innerHTML = ""; // Vide le contenu existant
+
+  // Ajouter les images filtrées au DOM
+  imagesFiltrees.forEach(function (image) {
+    const fig = document.createElement("figure");
+
+    const imgelement = document.createElement("img");
+    imgelement.src = image.imageUrl;
+    imgelement.alt = image.title;
+    imgelement.className = image.category.name;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerText = image.title;
+
+    fig.appendChild(imgelement);
+    fig.appendChild(figcaption);
+    container.appendChild(fig);
+  });
+}
+
+// Fonction pour gérer le clic sur les boutons et mettre à jour la classe "active"
+function gererCliqueBouton(button, images, categorie) {
+  button.addEventListener("click", function () {
+    // Supprimer la classe active de tous les boutons
+    const boutons = document.querySelectorAll(".btn");
+    boutons.forEach(function (act) {
+      act.classList.remove("active");
+    });
+
+    // Ajouter la classe active au bouton cliqué
+    button.classList.add("active");
+
+    // Afficher les images filtrées
+    afficherImagesFiltrees(images, categorie);
+  });
+}
+
+async function filtre() {
+  const images = await RecuperationImages();
+
+  const btnObjets = document.querySelector(".Objets");
+  const btnAppartements = document.querySelector(".Appartements");
+  const btnHotels = document.querySelector(".HotelsRestaurants");
+  const btnTous = document.querySelector(".Tous");
+
+  // Gérer les clics pour chaque bouton en appelant la fonction précéndente pour le button active
+  gererCliqueBouton(btnObjets, images, "Objets");
+  gererCliqueBouton(btnAppartements, images, "Appartements");
+  gererCliqueBouton(btnHotels, images, "Hotels & restaurants");
+  gererCliqueBouton(btnTous, images, null); // null pour afficher toutes les images
+}
+
+// Appel des fonctions
+RecuperationImages();
+filtre();
+
+// Récupérer les données du local storage
+//const userDataString = localStorage.getItem('userData');
+
+//if (userDataString) {
+//Convertir la chaîne JSON en objet JavaScript
+//   const userData = JSON.parse(userDataString);
+
+// Utiliser les données récupérées
+//   console.log('Données utilisateur:', userData);
+//} else {
+//    console.log('Aucune donnée trouvée dans le local storage');
+//}
---
+++ assets/login.js
@@ -0,0 +1,56 @@
+async function envoiLogin() {
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault(); // Empêche la soumission du formulaire
+
+      const mail = document.getElementById("mail").value;
+      const password = document.getElementById("password").value;
+
+      console.log("mail:", mail);
+      console.log("Mot de passe:", password);
+
+      try {
+        const response = await fetch(`http://localhost:5678/api/users/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: mail,
+            password: password,
+          }),
+        });
+
+        // Vérifiez la réponse du serveur
+        if (response.ok) {
+          const data = await response.json();
+          // stocker la response dans le local storage
+          localStorage.setItem("userData", JSON.stringify(data));
+          // Rediriger vers la page index
+          window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
+        } else {
+          console.error(
+            "Erreur lors de la requête:",
+            response.status,
+            response.statusText
+          );
+          // Vérifier si un message d'erreur existe déjà avec une boucle if
+          if (!document.getElementById("errorConnexion")) {
+            const container = document.getElementById("password");
+            const errorConnexion = document.createElement("p");
+            errorConnexion.id = "errorConnexion"; // Attribuer un id au message d'erreur
+            errorConnexion.textContent = `Erreur dans l’identifiant ou le mot de passe`;
+
+            // Insérer le message après l'élément de mot de passe
+            container.insertAdjacentElement("afterend", errorConnexion);
+          }
+        }
+      } catch (error) {
+        console.error("Erreur réseau ou autre:", error);
+      }
+    });
+}
+
+// Appel des fonctions
+envoiLogin();
